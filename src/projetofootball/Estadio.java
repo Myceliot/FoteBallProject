bd651e1fb6d4a90460515fd9b0178f4601fda438
Myceliot <mp0487346@gmail.com>
2024-10-21 14:51:13 +0100

    5 commit

--- src/projetofootball/Estadio.java
+++ src/projetofootball/Estadio.java
@@ -6,7 +6,7 @@ public class Estadio {
     private String designaçao;
     private String cidadeDePaisAnfritriao;
     private int lotaçaoMaxima;
-    private final static String DESEIGNACA_POR_OMISSAO = "Sem Dados";
+    private final static String DESIGNACAO_POR_OMISSAO = "Sem Dados";
     private final static String CIDADEDEPAISANFRITRIAO_POR_OMISSAO = "Sem Dados";
     private final static int LOTACAOMAXIMA_POR_OMISSAO = 0;
 
